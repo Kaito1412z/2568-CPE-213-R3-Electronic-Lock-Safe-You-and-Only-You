136b6a60d38c740873fb1b6c52a9524efe83aab0
Miki-tech-art <xenomotoam5@gmail.com>
2025-09-14 22:13:44 +0700

    Menu move to function void

---
+++ main_v4.cpp
@@ -0,0 +1,133 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+#define BUTTON_LEFT 27
+#define BUTTON_RIGHT 14
+#define BUTTON_BACK 12
+#define BUTTON_ENTER 13
+volatile unsigned long lastDebounceTime = 0;
+const unsigned long debounceDelay = 200;
+Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+enum state
+{
+  UNLOCKED,
+  LOCKED,
+  VIEW_LOG,
+  CHANGE_PASSWORD
+};
+volatile state CurrentState = UNLOCKED;
+//===================INTERRUPT BUTTON PIN==========================
+void IRAM_ATTR Left()
+{
+  if (millis() - lastDebounceTime >= debounceDelay)
+  {
+    lastDebounceTime = millis();
+    CurrentState = (state)(CurrentState - 1);
+    if (CurrentState == -1)
+    {
+      CurrentState = CHANGE_PASSWORD;
+    }
+  }
+}
+void IRAM_ATTR Right()
+{
+  if (millis() - lastDebounceTime >= debounceDelay)
+  {
+    lastDebounceTime = millis();
+    CurrentState = (state)(CurrentState + 1);
+    if (CurrentState == 4)
+    {
+      CurrentState = UNLOCKED;
+    }
+  }
+}
+//=======================DISPLAY STATE=============================
+void unlock()
+{
+  oled.clearDisplay();
+  oled.drawRect(2, 2, 124, 60, WHITE);
+  oled.setCursor(40, 20);
+  oled.println("UNLOCKED");
+
+  oled.setCursor(32, 35);
+  oled.println("Press Enter");
+}
+void locked()
+{
+  oled.clearDisplay();
+  oled.drawRect(2, 2, 124, 60, WHITE);
+  oled.setCursor(45, 20);
+  oled.println("LOCKED");
+  oled.setCursor(32, 35);
+  oled.println("Press Enter");
+}
+void chance_password()
+{
+  oled.clearDisplay();
+  oled.drawRect(2, 2, 124, 60, WHITE);
+  oled.setCursor(20, 20);
+  oled.println("CHANCE PASSWORD");
+  oled.setCursor(32, 35);
+  oled.println("Press Enter");
+}
+void log_view()
+{
+  oled.clearDisplay();
+  oled.drawRect(2, 2, 124, 60, WHITE);
+  oled.setCursor(40, 20);
+  oled.println("LOG VIEW");
+  oled.setCursor(32, 35);
+  oled.println("Press Enter");
+}
+
+
+void setup()
+{
+  Serial.begin(9600);
+
+  if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  {
+    Serial.println("SSD1306 allocation failed");
+    while (true)
+      ;
+  }
+
+  // PINMODE
+  pinMode(BUTTON_LEFT, INPUT_PULLUP);
+  pinMode(BUTTON_RIGHT, INPUT_PULLUP);
+  pinMode(BUTTON_ENTER, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(BUTTON_LEFT), Left, FALLING);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_RIGHT), Right, FALLING);
+
+  Serial.print("\n=================\nProject is Ready\n=================\n");
+}
+void loop()
+{
+
+  oled.setTextSize(1);
+  oled.setTextColor(WHITE);
+  oled.drawRect(5, 5, 118, 54, WHITE);
+  if (CurrentState == 0)
+  {
+    unlock(); //display
+  }
+  else if (CurrentState == 1)
+  {
+    locked(); //display
+  }
+  else if (CurrentState == 2)
+  {
+    chance_password(); //display
+  }
+  else if (CurrentState == 3)
+  {
+    log_view(); //display
+  }
+
+  oled.display();
+}
